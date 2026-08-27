9d780e9153f8ece915cea49b376d5810fe8b9b03
Twin <jarrettsneed2@gmail.com>
2026-08-26 19:10:49 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
-fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    // print!("$ ");
-    // io::stdout().flush().unwrap();
+fn main() -> anyhow::Result<()> {
+    print!("$ ");
+    io::stdout().flush()?;
+
+    Ok(())
 }
