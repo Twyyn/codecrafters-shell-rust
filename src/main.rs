5bf6e1802754128a1baec5652dd7e0105cb05da0
Twin <jarrettsneed2@gmail.com>
2026-08-26 19:14:50 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,13 @@ fn main() -> anyhow::Result<()> {
     print!("$ ");
     io::stdout().flush()?;
 
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)?;
+
+    // 3. Clean and process the input
+    let trimmed_input = input.trim();
+    println!("{trimmed_input}: command not found");
+
     Ok(())
 }
