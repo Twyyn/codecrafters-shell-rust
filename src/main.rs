d4771f1789c11c8a760593b6a3f8bfd2f0073d5f
Twin <jarrettsneed2@gmail.com>
2026-08-26 19:17:33 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,15 @@
 use std::io::{self, Write};
 
 fn main() -> anyhow::Result<()> {
-    print!("$ ");
-    io::stdout().flush()?;
+    loop {
+        print!("$ ");
+        io::stdout().flush()?;
 
-    let mut input = String::new();
+        let mut input = String::new();
 
-    io::stdin().read_line(&mut input)?;
+        io::stdin().read_line(&mut input)?;
 
-    // 3. Clean and process the input
-    let trimmed_input = input.trim();
-    println!("{trimmed_input}: command not found");
-
-    Ok(())
+        let trimmed_input = input.trim();
+        println!("{trimmed_input}: command not found");
+    }
 }
